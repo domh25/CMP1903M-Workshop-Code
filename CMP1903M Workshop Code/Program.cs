3020255d8167d540b2463c8dbe0e4f5466adebf4
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-02-18 16:45:22 +0000

    Add project files.

---
+++ CMP1903M Workshop Code/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CMP1903MWorkshopCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem 1
+            //(-4, -5) and (-1, -1) describe two points. What is the distance between them?
+            int x1 = -4;
+            int y1 = -4;
+
+            int x2 = -1;
+            int y2 = -1;
+
+            int answer1 = 0;
+
+            //Problem 2
+            //What is the denominator of the slope between these points?
+
+
+            //Problem 3
+            //What is the greatest common divisor (GCD) of 1124 and 136?
+
+
+            //Problem 4
+            //What is the remainder, when you divide the square of the sum of the first ten natural numbers by ten?
+        }
+    }
+}
