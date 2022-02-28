531c26a85af9b68ce54d7694b605c41230e63869
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-02-28 16:25:25 +0000

    Workshop 2

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -6,26 +6,59 @@ namespace CMP1903MWorkshopCode
     {
         static void Main(string[] args)
         {
-            //Problem 1
-            //(-4, -5) and (-1, -1) describe two points. What is the distance between them?
-            int x1 = -4;
-            int y1 = -4;
-
-            int x2 = -1;
-            int y2 = -1;
-
-            int answer1 = 0;
-
-            //Problem 2
-            //What is the denominator of the slope between these points?
+            //Week 2
+            //EITHER:
+            //Read in the text file if you want (or are able) [HINT: read into a list of strings]
+            //Split the first four numbers
+            List<string> streets = new List<string>();
 
 
-            //Problem 3
-            //What is the greatest common divisor (GCD) of 1124 and 136?
+
+            //OR:
+            //Manual (check each street code individually without reading them in automatically)
+            //long, tedious, etc, etc
+            //7778[8] is a correct street code
+            //Second two variables here are to use when calculating the checksum
+            int streetCode = 7778;
+            int checkSum = 8;
+            int total = 1;
+            int total2 = 0;
+          
+
+            //Multiply all the numbers together in the 4 digit code (7*7*7*8)
+            //Can do it by using the mod '%' operator and the divide '/' operator
+            //Reduce streetCode by 1 digit each time through the loop
+            while(streetCode > 0)
+            {
+                
+
+            }
 
 
-            //Problem 4
-            //What is the remainder, when you divide the square of the sum of the first ten natural numbers by ten?
+            //Add the digits of the multiplication total together
+            //infinite loop until the total gets to one digit - the checksum
+            //Use 'break;' to break out of the loop
+            while (true)
+            {
+                //Do the same as above, adding not multiplying
+                while (total > 0)
+                {
+                    
+
+                }
+
+                //Check if total2 is 2 digits or more
+                //If not, break out of the loop
+                if (total2 > 9) 
+                { 
+                    
+                }
+                else { break; }
+            }
+  
+            //Check if the final checksum is the same as the checksum in the street code.
+            
+
         }
     }
 }
